04c71abc7938095961b5bf8b7eab85a4e6854249
Lucas <lucas.thibaut.meyer@gmail.com>
2018-01-20 19:40:18 -0500

    Try to find the closest firemen barrack from the fire
    Remove entries in barracks list to avoid too many queries.
    Troubles with return of values in js.

--- website/js/script.js
+++ website/js/script.js
@@ -1,5 +1,49 @@
+function calculatDuration(directionsService, origin, destination, callback){
+    directionsService.route({
+        origin: origin,
+        destination: destination,
+        travelMode: 'DRIVING'
+    }, function(response, status){
+        if(status === 'OK'){
+            var totalDuration = 0;
+            var legs = response.routes[0].legs;
+            for(var i=0; i<legs.length; ++i) {
+                totalDuration += legs[i].duration.value;
+            }
+            var result = {origin: origin, duration: totalDuration};
+            callback(result);
+        }
+        else{
+            throw status;
+        }
+    });
+}
+
+function getClosestBarrack(directionsService, latLng, callback) {
+    $.getJSON("casernes.geojson", function(data){
+        var min_duration;
+        var closest_barrack;
+        var barracks = data["features"];
+        for (b in barracks){
+            var barrack_position = barracks[b]["geometry"]["coordinates"];
+            var barrack_latlng = new google.maps.LatLng({lat: barrack_position[1], lng: barrack_position[0]});
+            calculatDuration(directionsService, barrack_latlng, latLng, function(result){
+                var duration = result["duration"];
+                if(!min_duration | duration < min_duration){
+                    min_duration = duration;
+                    // closest_barrack is not assigned correctly
+                    closest_barrack = result["origin"];
+                }
+            });
+        }
+        console.log(closest_barrack);
+        callback(closest_barrack);
+    });
+}
+
 function initMap() {
     var montreal = {lat: 45.5052846, lng: -73.6116984};
+    var directionsService = new google.maps.DirectionsService;
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
         center: montreal,
@@ -49,7 +93,7 @@ function initMap() {
             });
         }
     });
-    
+
     // Fire icons on click
     map.addListener('click', function(event) {
         var fire_position = event.latLng;
@@ -57,6 +101,9 @@ function initMap() {
             position: fire_position,
             icon: fire_image,
             map: map
+        });
+        getClosestBarrack(directionsService, event.latLng, function(closest_barrack){
+            console.log(closest_barrack);
         });
         marker.addListener('click', function(){
             marker.setMap(null);
